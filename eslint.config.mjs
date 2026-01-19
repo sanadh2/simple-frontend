8ccec114498fe2b6d34de8de0c600f5f6fbfc6c1
Sanadh <sanadh.limenzy@gmail.com>
2026-01-19 15:16:15 +0530

    Update ESLint configuration to include TanStack Query plugin and rules
    - Added `@tanstack/eslint-plugin-query` to the ESLint configuration for enhanced query management.
    - Introduced new rules for TanStack Query: `@tanstack/query/exhaustive-deps`, `@tanstack/query/no-rest-destructuring`, and `@tanstack/query/stable-query-client` to enforce best practices in query usage.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
 import prettier from "eslint-plugin-prettier"
+import tanstackQuery from "@tanstack/eslint-plugin-query"
 
 const eslintConfig = defineConfig([
 	...nextVitals,
@@ -19,11 +20,15 @@ const eslintConfig = defineConfig([
 		plugins: {
 			"simple-import-sort": simpleImportSort,
 			prettier: prettier,
+			"@tanstack/query": tanstackQuery,
 		},
 		rules: {
 			"simple-import-sort/imports": "error",
 			"simple-import-sort/exports": "error",
 			"prettier/prettier": "error",
+			"@tanstack/query/exhaustive-deps": "warn",
+			"@tanstack/query/no-rest-destructuring": "warn",
+			"@tanstack/query/stable-query-client": "error",
 		},
 		extends: ["next/core-web-vitals", "prettier"],
 	},
